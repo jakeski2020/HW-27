fd0b1f2223600064ccc75ea5c843cf61bf02cab5
jakeski2020 <26joliviero@derryfield.org>
2026-04-01 09:53:08 -0400

    Move DemoSquare class to Geometry package

--- DemoSquare.java
+++ DemoSquare.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-package org.derryfield.math
+package org.derryfield.math.Geometry;
 public class DemoSquare {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
